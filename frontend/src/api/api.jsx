f328c5c701316808ed16e21dba3c338bf280a8d8
Bhumik Sorthiya <patelbhoomik860@gmail.com>
2026-08-06 15:47:19 +0530

    Backend API

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "https://amrut-jeevan-ayurveda-1.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
