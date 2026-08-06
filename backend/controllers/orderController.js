9612ab1359fbd750badaac3d481d55b3c08bca82
Bhumik Sorthiya <patelbhoomik860@gmail.com>
2026-08-06 16:32:19 +0530

    Google sheet Update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,10 @@
+import axios from "axios";
 import Order from "../models/Order.js";
 
 // ============================
 // Create New Order
 // ============================
-export const createOrder = async (req, res) => {
+eexport const createOrder = async (req, res) => {
   try {
     const { name, phone, city } = req.body;
 
@@ -14,17 +15,35 @@ export const createOrder = async (req, res) => {
       });
     }
 
+    // Save in MongoDB
     const order = await Order.create({
       name,
       phone,
       city,
     });
 
+    // Send to Google Sheets
+    try {
+      await axios.post(
+        "https://script.google.com/macros/s/AKfycbxvBjK_j2ifO89-shq7AKeQ2qTJuV87T4ya_HOd9uVfC3dtjNvJZrVuD43mmtrY12i_Qg/exec",
+        {
+          name,
+          phone,
+          city,
+        }
+      );
+
+      console.log("✅ Order sent to Google Sheets");
+    } catch (sheetError) {
+      console.error("Google Sheets Error:", sheetError.message);
+    }
+
     res.status(201).json({
       success: true,
       message: "Order created successfully.",
       data: order,
     });
+
   } catch (error) {
     console.error(error);
 
@@ -34,7 +53,6 @@ export const createOrder = async (req, res) => {
     });
   }
 };
-
 // ============================
 // Get All Orders
 // ============================
